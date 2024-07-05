ff95dcb0e604fb82f7932b53e84969b22d902edc
Anatoliy-ya <salamons777@gmail.com>
2024-07-05 13:43:59 +0300

    Add pagination for table devices

--- src/app/devices/devices.component.ts
+++ src/app/devices/devices.component.ts
@@ -4,27 +4,30 @@ import { formatDate } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { MatCard } from '@angular/material/card';
 import { ApiResponseInterface, DeviceInterface } from './devices.interfacel';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-devices',
   standalone: true,
-  imports: [MatCard, MatTableModule],
+  imports: [MatCard, MatTableModule, MatPaginator],
   templateUrl: './devices.component.html',
   styleUrls: ['./devices.component.scss'],
 })
 export class DevicesComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'last_active'];
   dataSource: DeviceInterface[] = [];
+  totalDevices = 0;
+  currentPage = 1;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.fetchDevices();
+    this.fetchDevices(this.currentPage);
   }
 
-  fetchDevices(): void {
+  fetchDevices(page: number): void {
     const requestPayload = {
-      page: 1,
+      page: page,
       last_page: 0,
       sort_field: 'id',
       sort: 'desc',
@@ -43,14 +46,21 @@ export class DevicesComponent implements OnInit {
       )
       .subscribe({
         next: (response: ApiResponseInterface) => {
+          console.log('Device fetch success', response);
           if (response && response.data && response.data.metering_devices) {
             this.dataSource = response.data.metering_devices.data;
+            this.totalDevices = response.data.metering_devices.total;
           }
         },
         error: (error) => {
           console.error('Device fetch error', error);
         },
       });
+  }
+
+  changePage(event: PageEvent): void {
+    this.currentPage = event.pageIndex + 1;
+    this.fetchDevices(this.currentPage);
   }
 
   formatDate(dateString: string): string {
